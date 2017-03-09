e055d75614a34e54f9419195c3d0b0b9a01cb83a
Innie Kim <innie.jong.kim@gmail.com>
2017-03-08 21:59:16 -0800

    Move public/dist to ../dist for smoother git adds

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,8 +35,8 @@ module.exports = function(grunt) {
       },
       client_and_lib: {
         files: {
-          'public/dist/build.js': ['public/client/app.js', 'public/client/link.js', 'public/client/links.js', 'public/client/linkView.js', 'public/client/linksView.js', 'public/client/createLinkView.js', 'public/client/router.js'],
-          'public/dist/lib.js': ['public/lib/jquery.js', 'public/lib/underscore.js', 'public/lib/backbone.js', 'public/lib/handlebars.js']
+          'dist/build.js': ['public/client/app.js', 'public/client/link.js', 'public/client/links.js', 'public/client/linkView.js', 'public/client/linksView.js', 'public/client/createLinkView.js', 'public/client/router.js'],
+          'dist/lib.js': ['public/lib/jquery.js', 'public/lib/underscore.js', 'public/lib/backbone.js', 'public/lib/handlebars.js']
         }
       }
     },
